0b8ef992db52bf595d663aa2f832124ec9c18ca4
Philipp Boersch-Supan <philipp@boersch-supan.de>
2022-08-17 09:30:46 +0100

    try parallel test to assess convergenc formally

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -2,18 +2,20 @@ data("sim_data_small")
 
 test_that("model runs through for moult in [-pi,pi]", {
 
-  uz2_circ_ufit <- uz2_circ("moult_score", "yday", data = sim_data_small[1:50,], chains = 1, refresh = 500)
+  uz2_circ_ufit <- uz2_circ("moult_score", "yday", data = sim_data_small[1:50,], chains = 2, parallel_chains = 2, refresh = 500)
   expect_s3_class(uz2_circ_ufit, "moultmcmc")
   #get quantiles
   q_days =quantile(rstan::extract(uz2_circ_ufit$stanfit,pars = "mu_days")$mu_days, p = c(0.025, 0.975))
   q_duration = quantile(rstan::extract(uz2_circ_ufit$stanfit,pars = "tau_days")$tau_days, p = c(0.025, 0.975))
   testthat::expect_equal(TRUE, dplyr::between(157, q_days[1],q_days[2]))
   testthat::expect_equal(TRUE, dplyr::between(102, q_duration[1],q_duration[2]))
+  testthat::expect_equal(TRUE, all(rstan::summary(uz2_circ_ufit$stanfit)$summary[,'Rhat']<1.1))
+
   })
 
 test_that("lumped model runs through for moult in [-pi,pi]", {
 
-  uz2_circ_lfit <- uz2_circ("moult_score", "yday", lump_non_moult = TRUE, data = sim_data_small[1:50,], chains = 1, refresh = 100, iter_warmup = 2000, iter_sampling = 2000)#very slow 35mins for 300iter
+  uz2_circ_lfit <- uz2_circ("moult_score", "yday", lump_non_moult = TRUE, data = sim_data_small[1:50,], chains = 1, refresh = 100, iter_warmup = 2000, iter_sampling = 2000, chains = 2, parallel_chains = 2)#very slow 35mins for 300iter
   expect_s3_class(uz2_circ_lfit, "moultmcmc")
   #get quantiles
   q_days =quantile(rstan::extract(uz2_circ_lfit$stanfit,pars = "mu_days")$mu_days, p = c(0.025, 0.975))
@@ -22,4 +24,6 @@ test_that("lumped model runs through for moult in [-pi,pi]", {
   message(paste(q_duration, 'days'))
   testthat::expect_equal(TRUE, dplyr::between(157, q_days[1],q_days[2]))
   testthat::expect_equal(TRUE, dplyr::between(102, q_duration[1],q_duration[2]))
+  testthat::expect_equal(TRUE, all(rstan::summary(uz2_circ_lfit$stanfit)$summary[,'Rhat']<1.1))
+
 })
