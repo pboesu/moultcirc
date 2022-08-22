d33521e5f2bb9284f3b3452e0d616955cdf3002b
Philipp Boersch-Supan <philipp.boerschsupan@bto.org>
2022-08-22 09:05:23 +0100

    rerun benchmark

--- sandbox/benchmark_stan_vectorisation.R
+++ sandbox/benchmark_stan_vectorisation.R
@@ -12,7 +12,7 @@ mbm = microbenchmark(optT = uz2_circ("moult_score", "yday", data = sim_data_smal
                 optF1k = uz2_circ("moult_score", "yday", data = sim_data_small, chains = 2, parallel_chains = 2, refresh = 1000, opt = FALSE),
                 optTs = uz2_circ("moult_score", "yday_shifted", data = sim_data_small[1:100,], chains = 2, parallel_chains = 2, refresh = 1000),
                 optFs = uz2_circ("moult_score", "yday_shifted", data = sim_data_small[1:100,], chains = 2, parallel_chains = 2, refresh = 1000, opt = FALSE),
-                times = 10)
+                times = 20)
 
 saveRDS(mbm, 'sandbox/mbm_uz2_circ.rds')
 pdf('sandbox/mbm_uz2_circ.pdf')
